4992d0be841b2f7aacd7e545248240e8f8a37527
Brandon Duncan <90273289+BrandonDuncan13@users.noreply.github.com>
2022-06-01 18:00:01 -0600

    Bottom Slider Progress
    Added the bottom slider with working cancel button. Still bugs to fix.

--- screens/anotherScreen.js
+++ screens/anotherScreen.js
@@ -2,29 +2,82 @@
 import React from 'react';
 import { View, StyleSheet, ImageBackground, Text, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
-export default function anotherScreen() {
+import BottomSheet from 'reanimated-bottom-sheet';
+import Animated from 'react-native-reanimated';
 
-    const image = require('../images/bigblossoms.jpg');
+const AnotherScreen = () => {
 
-        return (
-            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-                <View style={styles.container}>
-                    <ImageBackground
-                    style={styles.backgroundImage}
-                    resizeMode="cover"
-                    source={image}
-                    >
-                        <TouchableOpacity
-                        style={styles.textBox}
-                        activeOpacity={0.8}
-                        >
-                            <Text style={styles.text}>Blossoming Apple Tree</Text>
-                        </TouchableOpacity>
-                    </ImageBackground>
-                </View>
-            </TouchableWithoutFeedback>
-            );
-        }
+  const image = require('../images/bigblossoms.jpg');
+
+  const renderInner = () => (
+    <View style={styles.panel}>
+      <View style={styles.centerTitle}>
+        <Text style={styles.panelTitle}>Upload Photo</Text>
+        <Text style={styles.panelSubtitle}>Choose Your Profile Picture</Text>
+      </View>
+      <TouchableOpacity style={styles.panelButton}>
+        <Text style={styles.panelButtonTitle}>Take Photo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.panelButton}>
+        <Text style={styles.panelButtonTitle}>Choose From Library</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.panelButton}
+        onPress={() => bs.current.snapTo(1)}>
+        <Text style={styles.panelButtonTitle}>Cancel</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  const renderHeader = () => (
+    <View style={styles.header}>
+      <View style={styles.panelHeader}>
+        <View style={styles.panelHandle} />
+      </View>
+    </View>
+  );
+
+  let bs = React.createRef();
+  let fall = new Animated.Value(1);
+
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <BottomSheet
+            ref={bs}
+            snapPoints={[445, 0]}
+            renderContent={renderInner}
+            renderHeader={renderHeader}
+            initialSnap={1}
+            callbackNode={fall}
+            enabledGestureInteraction={true}
+
+        />
+        <Animated.View style={{opacity: Animated.add(0.4, Animated.multiply(fall, 1.0)),
+        flex: 1,
+        }}>
+          <View style={styles.bgWrapper}>
+          <ImageBackground
+            style={styles.backgroundImage}
+            resizeMode="cover"
+            source={image}
+          >
+            <TouchableOpacity
+            style={styles.textBox}
+            activeOpacity={0.8}
+            onPress={() => bs.current.snapTo(0)}
+            >
+              <Text style={styles.text}>Blossoming Apple Tree</Text>
+            </TouchableOpacity>
+          </ImageBackground>
+          </View>
+        </Animated.View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default AnotherScreen;
 
 const styles = StyleSheet.create({
     container: {
@@ -73,5 +126,38 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         backgroundColor: '#00000040',
         marginBottom: 10,
+    },
+    panel: {
+      padding: 20,
+      backgroundColor: '#FFFFFF',
+      paddingTop: 20,
+    },
+    panelTitle: {
+      fontSize: 27,
+      height: 35,
+    },
+    panelSubtitle: {
+      fontSize: 14,
+      color: 'gray',
+      height: 30,
+      marginBottom: 10,
+    },
+    panelButton: {
+      padding: 13,
+      borderRadius: 10,
+      backgroundColor: '#FF6347',
+      alignItems: 'center',
+      marginVertical: 7,
+    },
+    panelButtonTitle: {
+      fontSize: 17,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+    centerTitle: {
+      alignItems: 'center',
+    },
+    bgWrapper: {
+      margin: 0,
     },
   });
