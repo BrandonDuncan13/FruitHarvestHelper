7d576257a59f87a395f98b0d1f1f2027315026b9
BatmanNelson <10joshn@gmail.com>
2022-07-13 19:56:44 -0600

    opencv works now
    I will delete this branch later, it is only temporary

--- src/cpp/process_image.cpp
+++ src/cpp/process_image.cpp
@@ -1,16 +1,27 @@
 #include "process_image.hpp"
+// #ifdef __cplusplus
+// #import <opencv2/opencv.hpp>
+// #endif
+
+// #include <opencv2/highgui/highgui.hpp>
+// #include <opencv2/core/core.hpp>
 
 
 // Constructor
-int ProcessImage::ProcessImageImpl()
-{
-    return 0;
-}
+// int ProcessImage::ProcessImageImpl()
+// {
+//     numBlossoms = 404;
+//     return 0;
+// }
 
 // Here is where the image gets processed
 std::string ProcessImage::get_processed_image()
 {
-    int numBlossoms = 404;
+    cv::Mat inputMat = cv::Mat::zeros(2, 2, CV_32F);
+    inputMat.at<unsigned char>(1,1) = 3;
+    cv::Mat greyMat;
+    // cv::cvtColor(inputMat, greyMat, cv::COLOR_BGR2GRAY);
+    numBlossoms = 404;
     std::string myString = std::to_string(numBlossoms) + "$$image_path";
     return myString;
 }
--- src/cpp/process_image.hpp
+++ src/cpp/process_image.hpp
@@ -7,9 +7,14 @@ class ProcessImage
 {
 public:
 
-    // Constructor
-    int ProcessImageImpl();
+    // // Constructor
+    // int ProcessImageImpl();
 
     // Our method that returns a string
     std::string get_processed_image();
+
+    private:
+
+    // Variables
+    int numBlossoms;
 };
