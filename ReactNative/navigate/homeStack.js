5648d0c612df854576d0fb3ba48526f7800f5acb
Brandon Duncan <90273289+BrandonDuncan13@users.noreply.github.com>
2024-06-26 14:34:21 -0600

    Code Cleanup
    Made some better comments and got rid of code that wasn't useful.

--- ReactNative/navigate/homeStack.js
+++ ReactNative/navigate/homeStack.js
@@ -1,12 +1,7 @@
 /* eslint-disable prettier/prettier */
-/*
-npx react-native run-android
-*/
 import React from 'react';
 
-// These two things make up the "Stack Navigator" and must be imported for each stack navigator you want to create
-// some apps use multiple stack navigators but most apps use one stack navigator and one drawer navigator
-// this uses the native stack navigator from React Navigation a third party navigation source
+// App uses the native stack navigator from React Navigation a third party navigation source
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -15,20 +10,17 @@ import DetectApples from '../screens/detectApples';
 
 const Stack = createNativeStackNavigator();
 
-// there's no StyleSheet in this file becuase the styling is done within the Components that are configured inside the navigator
-// here there is a class component called HomeStack which holds the navigation layout and logic for the app
+// Class component called HomeStack holds the navigation layout and logic for the app
 const HomeStack = () => {
   return (
     <NavigationContainer>
       {/* The navigation container must be wrapped around the Stack Navigator in order to keep the app's state */}
       <Stack.Navigator
-      /* The Stack Navigator provides a way to transition between screens where each new screen is placed
-      onto the top of the stack. Stacks can also be popped off the stack by clicking the back arrow, etc. */
+      // Stack Navigator is a stack that allows user to switch between screens -> display screen on top of the stack
         screenOptions={{
-          // screen options offers customization options for the navigation header, items belonging to the header, etc.
+          // Customization options for the header and items inside the header
           headerStyle: {
-            backgroundColor: '#1a1a1c',
-            //backgroundColor: '#111112',
+            backgroundColor: '#1a1a1c'
           },
           headerTitleStyle: {
             color: 'white',
@@ -37,18 +29,15 @@ const HomeStack = () => {
           headerTitleAlign: 'center',
         }}>
         <Stack.Screen
-        /* The Home Component is placed as the first Stack.Screen which makes it the default
-        screen that appears on the device. A Stack.Screen is a screen that can be added to the stack
-        when an event triggers */
-          name="Home" // The name is used for navigating to this Stack.Screen from another screen
-          component={Home} // this Stack.Screen is configuring the Home component
+        // Stack screens are screens that can be added to the stack -> Home screen is first so it's default screen
+          name="Home" // Name used to navigate to the screen from another screen
+          component={Home}
           options={{
-            title: 'Apple Detection App', // The title of the screen in the navigation heading
+            title: 'Apple Detection App', // Title of screen in the navigation heading
           }}
         />
         <Stack.Screen
-        /* Since the Detect Apples screen isn't the first Stack.Screen to appear in the code
-        it won't be the default screen and will have to be navigated to in order to access it */
+        // Screen to navigate to
           name="Detect Apples"
           component={DetectApples}
           options={{
