2ea2128f7db152fd38acb64e7ea30f831c86b647
Brandon Duncan <90273289+BrandonDuncan13@users.noreply.github.com>
2024-06-28 23:23:02 -0600

    Cleaned-up App.js File

--- ReactNative/App.js
+++ ReactNative/App.js
@@ -1,5 +1,3 @@
-// this command is on every page just to make running the code easier
-
 // IMPORTANT: this project uses the bare react native cli not the expo cli
 /* IMPORTANT: a library called 'react-native-version' was installed
 Modify the package.json file's version and run the command 'react-native-version --never-amend' in React Native
